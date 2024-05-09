972ed3851f2a71d165b22bb7d2e38ec2ad29db94
SongYibao <songybmail@foxmail.com>
2024-05-09 19:27:55 +0800

    update: 更新编译脚本

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ rm -rf ./build
 mkdir build && cd build && cmake .. && make
 echo "build done,coping plugin ..."
 cp -r ./libplugin-mqtt-quic.so /home/songyibao/Downloads/neuron-main/build/plugins/
+# 拷贝 .so 和 json配置 文件到 系统安装的 neuron 目录
+cp -r ./libplugin-mqtt-quic.so /opt/neuron/plugins/
+cp -r ../mqtt-quic.json /opt/neuron/plugins/schema/
 echo "copy done, refresh http://127.0.0.1:7000"
